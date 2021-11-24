5e65409dba0b1af603cd192688ed131ee303e150
helinzai <1136871076@qq.com>
2021-11-24 19:11:31 +0800

    [Monitor_calcu] add MacBook_Air_2020
    Signed-off-by: helinzai <1136871076@qq.com>

--- Monitor_calcu/src/main.cpp
+++ Monitor_calcu/src/main.cpp
@@ -6,11 +6,14 @@ int main() {
 	cout << "-------------------------------------------" << endl << endl;
 
 	//already added:
-	//R7000P,Y410P,R9000P
-	Monitor R7000P,Y410P,R9000P;
+	//R7000P,Y410P,R9000P,MacBook_Air_2020
+	Monitor R7000P,Y410P,R9000P,MacBook_Air_2020;
 	R7000P.set_monitor("R7000P", 1920, 1080, 15.6);
 	Y410P.set_monitor("Y410P", 1600, 900, 14);
 	R9000P.set_monitor("R9000P", 2560, 1600, 16.1);
+	MacBook_Air_2020.set_monitor("MacBook_Air_2020", 2560, 1600, 13.3);
 
-	R9000P.show_all_info();
+	//R9000P.show_all_info();
+	Y410P.show_all_info();
+	MacBook_Air_2020.show_all_info();
 }
