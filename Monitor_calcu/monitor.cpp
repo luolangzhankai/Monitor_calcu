544fa0a5f621f7674ef3d3b843736592d05ab891
helinzai <1136871076@qq.com>
2021-11-24 16:22:16 +0800

    [Monitor_calcu] add show_monitor_ratio()
    Signed-off-by: helinzai <1136871076@qq.com>

--- Monitor_calcu/monitor.cpp
+++ Monitor_calcu/monitor.cpp
@@ -1,1 +1,39 @@
 //for funtion of monitor
+
+#include"monitor.h"
+
+void Monitor::set_monitor(string name, double w, double h, double size) {
+	this->w_pixel = w;
+	this->h_pixel = h;
+	this->size = size;
+	this->monitor_name = name;
+}
+
+void Monitor::show_monitor() {
+	double ratio = w_pixel / h_pixel;
+	int monitor_ratio = (int)(ratio * 10);
+	cout << monitor_name << " 's monitor w/h ratio is: ";
+	switch (monitor_ratio) {
+	case 35:
+		cout << "32:9 " << endl;
+		break;
+	case 23:
+		cout << "21:9 " << endl;
+		break;
+	case 20:
+		cout << "18:9 " << endl;
+		break;
+	case 17:
+		cout << "16:9 " << endl;
+		break;
+	case 16:
+		cout << "16:10 " << endl;
+		break;
+	case 15:
+		cout << "3:2 " << endl;
+		break;
+	case 13:
+		cout << "4:3 " << endl;
+		break;
+	}
+}
--- Monitor_calcu/monitor.h
+++ Monitor_calcu/monitor.h
@@ -1,1 +1,17 @@
-#define hello_word "hello word";
+// .h for monitor.cpp
+#define welcome_word "---- Use this project to calculate you monitor for more infomation. ----"
+
+#include<iostream>
+using namespace std;
+class Monitor {
+public:
+	string monitor_name;
+	Monitor() {}
+	void set_monitor(string name, double w, double h, double size);
+	void show_monitor();
+
+private:
+	double w_pixel;
+	double h_pixel;
+	double size;
+};
